985edfe099d8978a9e2d4d796ab7ced7c5473f84
Johannes Reuter <jreuter@inovex.de>
2015-10-29 08:37:31 +0100

    added saucelabs testing

--- wct.conf.js
+++ wct.conf.js
@@ -2,7 +2,14 @@
 module.exports = {
   plugins: {
     local: {
-      browsers: ['firefox']
+      browsers: ['firefox'],
+    },
+    /* To use this, you have to specify SAUCE_USERNAME and SAUCE_ACCESS_KEY, e.g.
+    export SAUCE_USERNAME=abc123
+    export SAUCE_ACCESS_KEY=52345s3-5d32345-234-d5-5345f34
+    */
+    sauce: {
+      browsers: ['Windows 7/internet explorer']
     }
   },
 };
